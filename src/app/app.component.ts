b64e09f482a806ec2bf728750093a332c52e11ec
bdhanpal001 <bdhanpal001@US1INF084D70108.us.nam.ad.pwcinternal.com>
2017-09-18 04:42:33 -0400

    login changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1> <login-page></login-page>`,
 })
 export class AppComponent  { name = 'Angular'; }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login-page',
+  templateUrl: `./login.html`,
+})
+export class LoginComponent  { name = 'Angular'; }

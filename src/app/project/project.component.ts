cdc1c612e279efdd32a1ec01289a2cc204b6b3de
bdhanpal001 <bdhanpal001@US1INF084D70108.us.nam.ad.pwcinternal.com>
2017-09-26 09:37:57 -0400

    PROJECT ADDED--TEAM,SPRINT AND dOCUMENT IS ADDED

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -9,33 +9,41 @@ import { DataFilterPipe }   from './data-filter.pipe';
   templateUrl: `./project.html`,
    providers: [ProjectService]
 })
-export class ProjectComponent  { 
+export class ProjectComponent  {
     id: any;
   projectinfo:any;
   projectinfo1:any;
   public data:any;
+  childproject:any;
   constructor(private _routeParams:  ActivatedRoute,private projectservice: ProjectService,private router: Router
         ) {
   }
  ngOnInit() {
        this.projectinfo=this._routeParams.url;
-          this.getprojects();
-   this.projectinfo1=true;
-    this.projectinfo = this._routeParams.params.subscribe(params => {
-      debugger;
-       this.id = +params['id']; // (+) converts string 'id' to a number
-this.projectinfo1=this.id;
-      if(!this.id){
-      this.projectinfo1=false;
+       this.getprojects();
+       this.projectinfo1 = true;
+       this.projectinfo = this._routeParams.params.subscribe(params => {
+       this.id = params['id']; // (+) converts string 'id' to a number
+       this.projectinfo1 = this.id;
+      if(this.projectinfo1.search('_')==1){
+      let childnode=this.projectinfo1.split('_');
+        this.childproject = childnode[1];
+        this.projectinfo1 = childnode[0];
       }
-     
+         else{
+       this.childproject = false;
+      }
+      if(!this.id) {
+      this.projectinfo1 = false;
+      }
+
     });
   }
 
   getprojects() {
         this.projectservice.getProjectListdetails().subscribe((response:any) => { // <---
         this.data = response;
-        
+
     }, (err:any) => { // <---
         console.log(err);
     }
